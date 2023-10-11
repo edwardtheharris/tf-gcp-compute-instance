17132f12f60ebcc888e69590d85f346479763b46
Xander Harris <xander.harris@wunderkind.co>
2023-10-11 08:39:07 -0700

    Add Docker installation
    8-fix-the-boot-disk-size
    Closes #8
    
    @xander.harris
    
    Changelog: changed

---
+++ _scripts/install-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
+    apt-get remove -y $pkg;
+done
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
