92c5fd2164014bd083c94277401b0bde6a495023
Xander Harris <xander.harris@wunderkind.co>
2023-10-16 10:03:53 -0700

    Update nc usage
    26-get-nc-installed-on-the-runners
    Closes #26
    
    @xander.harris
    
    Changelog: changed

--- _scripts/wait-for-ssh.sh
+++ _scripts/wait-for-ssh.sh
@@ -2,11 +2,6 @@
 
 REMOTE=$1
 
-if [[ -f $(command -v apt-get) ]]; then
-  sudo apt-get -y update
-  sudo apt-get -y install netcat
-fi
-
 while ! nc -z "${REMOTE}" 22; do
   printf "Waiting for ssh connection. . . \n"
   sleep 0.1 # wait for 1/10 of the second before check again
