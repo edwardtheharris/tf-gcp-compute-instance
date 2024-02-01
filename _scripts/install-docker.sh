cd57e986d6aa92e0eb216bb0747b8a9cbb24758b
Xander Harris <xandertheharris@gmail.com>
2024-01-31 18:59:16 -0800

    install kubernetes tools
    See #43
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- _scripts/install-docker.sh
+++ _scripts/install-docker.sh
@@ -62,3 +62,22 @@ echo -n "${LOCAL_PUBLIC_KEY}" | base64 -d >> ~/.ssh/authorized_keys
 echo -n "${LOCAL_PUBLIC_KEY}" | base64 -d >> ~/.ssh/id_rsa.pub
 echo -n "${LOCAL_PRIVATE_KEY}" | base64 -d > ~/.ssh/id_rsa
 scp
+
+sudo apt install -y curl wget apt-transport-https
+
+cd "$HOME" || true
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
+# If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
